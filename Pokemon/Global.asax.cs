ca777f264a6aa227cfae108d1b4fc9524731df57
Ronald Mora Gómez <ronald.mora@ucrso.info>
2021-05-28 11:54:56 -0600

    ConnectionString Added

--- Pokemon/Global.asax.cs
+++ Pokemon/Global.asax.cs
@@ -16,6 +16,7 @@ namespace Pokemon
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
         }
     }
 }
